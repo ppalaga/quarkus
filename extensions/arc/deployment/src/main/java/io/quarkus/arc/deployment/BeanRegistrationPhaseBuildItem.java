d1b14ea523bae69d956a7bf6cdb2066cc80e02b9
Peter Palaga <ppalaga@redhat.com>
2019-11-11 10:58:55 +0100

    Make BeanRegistrationPhaseBuildItem.getBeanProcessor() public

--- extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/BeanRegistrationPhaseBuildItem.java
+++ extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/BeanRegistrationPhaseBuildItem.java
@@ -16,7 +16,7 @@ import io.quarkus.deployment.annotations.BuildProducer;
  * build step should produce a {@link BeanConfiguratorBuildItem} or at least inject a {@link BuildProducer} for this build item,
  * otherwise it could be ignored or processed at the wrong time, e.g. after
  * {@link ArcProcessor#validate(BeanRegistrationPhaseBuildItem, List)}.
- * 
+ *
  * @see BeanConfiguratorBuildItem
  */
 public final class BeanRegistrationPhaseBuildItem extends SimpleBuildItem {
@@ -33,7 +33,7 @@ public final class BeanRegistrationPhaseBuildItem extends SimpleBuildItem {
         return context;
     }
 
-    BeanProcessor getBeanProcessor() {
+    public BeanProcessor getBeanProcessor() {
         return beanProcessor;
     }
 
